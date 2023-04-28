6ec5230410dc6ef607e7d266348ee2397d778605
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-04-28 09:25:04 +0800

    First upload

---
+++ TCP/Code/Command.py
@@ -0,0 +1,14 @@
+class COMMAND:
+
+    CMD_MOTOR      =  "M"
+    CMD_LED        =  "L"
+    CMD_LED_MOD    =  "D"
+    CMD_SERVO      =  "S"
+    CMD_BUZZER     =  "B"
+    CMD_POWER      =  "P"
+    CMD_MATRIX_MOD =  "T"
+    CMD_CAR_MODE   =  "C"
+
+    def __init__(self):
+        pass
+
