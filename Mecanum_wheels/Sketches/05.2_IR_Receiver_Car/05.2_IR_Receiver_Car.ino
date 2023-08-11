11f55cc4d6d00267814da3eef1ebdf5dc36253c8
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-08-11 14:16:18 +0800

    Update 05.2_IR_Receiver_Car.ino

--- Mecanum_wheels/Sketches/05.2_IR_Receiver_Car/05.2_IR_Receiver_Car.ino
+++ Mecanum_wheels/Sketches/05.2_IR_Receiver_Car/05.2_IR_Receiver_Car.ino
@@ -12,10 +12,11 @@
 #define DISABLE_LED_FEEDBACK false
 int motor_speed = 50;
 int emotionMode = 4;
-float time_proportion = 5.7;        //If you want to get the best out of the rotation mode, change the value by experimenting
+float time_proportion = 5.7;  //If you want to get the best out of the rotation mode, change the value by experimenting
 int motor_flag = 0;
 int set_angle = 0;  //Set the forward angle of rotation
-int rotate_flag = 0;  
+int rotate_flag = 0;
+int around_rotate_flag = 0;
 float battery_voltage = 0;
 
 void setup() {
@@ -34,19 +35,31 @@ void loop() {
     IrReceiver.resume();  // Enable receiving of the next value
   }
   showEmotion(emotionMode);
-  if (rotate_flag) {    //Rotate when the rotate_glag is true
+  if (rotate_flag) {  //Rotate when the rotate_glag is true
     int VY = 40 * cos(set_angle * (M_PI / 180));
     int VX = -(40 * sin(set_angle * (M_PI / 180)));
     int W = 40;
 
-    int FR = VY - VX + W; 
-    int FL = VY + VX - W; 
+    int FR = VY - VX + W;
+    int FL = VY + VX - W;
     int BL = VY - VX - W;
     int BR = VY + VX + W;
 
     Motor_M_Move(FL, BL, BR, FR);
     delay(5 * time_proportion * 8 / battery_voltage);
     set_angle -= 5;
+  }
+  //The car rotates around a point
+  if (around_rotate_flag) {                     //Rotate when the rotate_glag is true 围绕旋转模式//-90 100 90 100
+    int LY = 100 * cos(-90 * (M_PI / 180));     //paramters[1] represents the Angle to the the Y-axis,Counterclockwise is 0 to 180 degrees
+    int LX = -(100 * sin(-90 * (M_PI / 180)));  //paramters[2] represents the move speed(the first jostick)
+    int RX = 100 * sin(90 * (M_PI / 180));      //paramters[3] represents the Angle to the Y-axis,Counterclockwise is 0 to 180 degrees
+    int RY = 100 * cos(90 * (M_PI / 180));      //Converts data from the client to its x and y axis positions
+    int FR = LY - LX + RX;  //The McNamum wheel chassis motion formula
+    int FL = LY + LX - RX;  //LY stands for longitudinal velocity
+    int BL = LY - LX - RX;  //LX stands for transverse velocity
+    int BR = LY + LX + RX;  //RX stands for angular velocity
+    Motor_M_Move(FL, BL, BR, FR);
   }
 }
 
@@ -150,6 +163,10 @@ void handleControl(unsigned long value) {
       battery_voltage = Get_Battery_Voltage();
       rotate_flag = !rotate_flag;
       set_angle = 0;
+      break;
+
+    case 0xE718FF00:  // Receive the number '2'
+      around_rotate_flag = !around_rotate_flag;
       break;
 
     default:
